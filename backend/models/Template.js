67149f0f2e6f31bb43b3e411a76031747d17324a
ulagu2007 <ulagan2007@gmail.com>
2025-05-05 22:35:50 +0530

    Add backend/models/Template.js 1746464816766

---
+++ backend/models/Template.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const templateSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  content: { type: String, required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Template', templateSchema);
